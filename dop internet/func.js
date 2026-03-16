3accf7a8571041900b7fc5b6860ca7863543d290
admin1350 <147304099+admin1350@users.noreply.github.com>
2026-03-16 09:21:03 +0000

    add lab4 file test ver

---
+++ dop internet/func.js
@@ -0,0 +1,24 @@
+
+
+
+var value1 = "40";
+var min1 = "-40";
+var max1 = "40";
+function isValidReading(value1, min1, max1){
+
+}
+
+var data2 = "test";
+var min2 = "tset"; 
+var max2 = "40";
+function filterInvalidReadings(data, min, max){}
+
+var data3 = "test";
+var min3 = "tset"; 
+var max3 = "40";
+function replaceInvalidWithAverage(data3, min3, max3){}
+
+var data4 = "test";
+var min4 = "tset"; 
+var max4  = "40";
+function  analyzeDataQualit(data4, min4, max4){}
