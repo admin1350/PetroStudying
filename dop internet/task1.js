68b1378d49d74c7685674ac355075a30b5f3be09
admin1350 <wwwcat2008@gmail.com>
2026-04-27 00:50:03 +0300

    task1

--- dop internet/task1.js
+++ dop internet/task1.js
@@ -2,4 +2,5 @@ let temp1 = 18;
 let temp2 = 23;
 let temp3 = 21;
 
-let srtemp = (temp1 + temp2 + temp3)
+let srtemp = (temp1 + temp2 + temp3);
+console.log(`Средняя температура: ${srtemp.toFixed(2)}°C`);
